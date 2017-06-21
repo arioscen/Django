b3ee25e1d90c5cebcbe7bfe334a3c34676a66770
arioscen <arioscen@gmail.com>
2017-06-21 13:54:38 +0800

    create base.html & connect post.id on post.title

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<id>\d+)', views.post_record,name='post_record'),
+    # url(r'^(?P<id>\d+)', views.post_record,name='post_record'),
+    url(r'^(\d+)', views.post_record,name='post_record'),
     url(r'^add_record',views.add_record, name='add_record'),
     url(r'^$', views.post_list, name='post_list'),
 ]
