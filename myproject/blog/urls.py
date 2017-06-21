7daac931a6fabbdaa1ad4793ceacc4591f532c9f
arioscen <arioscen@gmail.com>
2017-06-21 10:33:08 +0800

    add form

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^add_record',views.add_record, name='add_record'),
     url(r'^$', views.post_list, name='post_list'),
 ]
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+me = User.objects.get(username="iii")
 
 # def post_list(request):
 #     return render(request, 'blog/post_list.html', {})
 
 def post_list(request):
     # posts = Post.objects.all().order_by('created_date')
-    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    # posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
+    # return render(request, 'blog/post_list.html', {'posts':posts})
+
+    post_form = PostForm()
+    return render(request, 'blog/post_list.html', locals())
+
+def add_record(request):
+    if request.POST:
+        title = request.POST['title'].encode('utf-8')
+        text = request.POST['text'].encode('utf-8')
+        newpost = Post.objects.create(author=me,title=title,text=text)
+    return redirect('/blog')
